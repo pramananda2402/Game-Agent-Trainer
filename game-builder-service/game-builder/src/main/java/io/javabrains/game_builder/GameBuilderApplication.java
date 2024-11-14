c511aed2c0d328da8b7ccf0f851cce53ed954fe6
pramananda <pramananda2402@gamil.com>
2024-11-14 20:42:31 +0600

    game builder spring boot dir added

---
+++ game-builder-service/game-builder/src/main/java/io/javabrains/game_builder/GameBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.game_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBuilderApplication.class, args);
+	}
+
+}
---
+++ game-builder-service/game-builder/src/test/java/io/javabrains/game_builder/GameBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.game_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
